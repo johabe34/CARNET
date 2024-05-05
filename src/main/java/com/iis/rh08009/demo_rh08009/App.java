f5a703efa99fc8b3408bd28f7c2a92a907b3809d
johabe34 <johanna.rohdz@gmail.com>
2024-05-04 18:02:49 -0600

    cambios
    nuevo

---
+++ src/main/java/com/iis/rh08009/demo_rh08009/App.java
@@ -0,0 +1,17 @@
+package com.iis.rh08009.demo_rh08009;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hola";
+	}
+    public static void main( String[] args )
+    {
+        App hw = new App();
+    	System.out.println( hw.Hello());
+    }
+}
